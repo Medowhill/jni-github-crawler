4ace9ef89ea09d1f7f81828cc3973d153fc57b10
Jihee Park <j31d0@users.noreply.github.com>
2021-12-14 15:20:08 +0900

    Save/Load feature

--- src/main/scala/info/hjaem/github/Main.scala
+++ src/main/scala/info/hjaem/github/Main.scala
@@ -8,33 +8,71 @@ import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
 import spray.json._
+import scala.collection.mutable.Queue
+import java.io._
 
 object Main {
 
   val browser = JsoupBrowser()
   var isFirst = true
   val sleepSeconds = 10
+  var saveCount = 0
+  val saveFileName = "save.dump"
 
   def main(args: Array[String]): Unit = args match {
     case Array(start, end, lang) =>
-      val repos = (start.toInt to end.toInt).to(LazyList)
-        .flatMap(getRepositories(_, lang))
-      val jniRepos = repos.filter(_.jni)
-      jniRepos foreach println
-      println(repos.length)
-      println(jniRepos.length)
+      doA(start.toInt, end.toInt, lang)
+    case Array("resume") =>
+      val (pages, repos, results, lang) = load
+      doResume(repos, pages, results, lang)
     case _ =>
       println("usage: run [pages] [lang]")
   }
 
-  def getRepositories(page: Int, lang: String): List[Repository] = {
-    if (isFirst) isFirst = false else Thread.sleep(sleepSeconds * 1000)
-    val url = s"https://api.github.com/search/repositories?order=desc&page=$page&q=language:$lang&sort=stars"
-    get(url, Repository.headers).parseJson.asJsObject.fields("items").asInstanceOf[JsArray].elements.map(
-      (x) => Repository(x.asJsObject.fields("url").asInstanceOf[JsString].value)).toList
+  def doA(start: Int, end: Int, lang: String) {
+    val repos = Queue[String]()
+    val pages = (start.toInt to end.toInt).to(Queue)
+    val results = Queue[String]()
+    doResume(repos, pages, results, lang)
   }
 
-  def get(url: String, headers: Map[String, String] = Map()): String = {
+  def trySave(x: (Queue[Int], Queue[String], Queue[String], String)) {
+    saveCount += 1
+    if (saveCount % 100 == 0) {
+      val oos = new ObjectOutputStream(new FileOutputStream(saveFileName))
+      oos.writeObject(x)
+      oos.close
+    }
+  }
+
+  def load: (Queue[Int], Queue[String], Queue[String], String) = {
+    val ois = new ObjectInputStream(new FileInputStream(saveFileName))
+    ois.readObject().asInstanceOf[(Queue[Int], Queue[String], Queue[String], String)]
+  }
+
+  def doResume(repos: Queue[String], pages: Queue[Int], results: Queue[String], lang: String) {
+    while (!pages.isEmpty) {
+      while (!repos.isEmpty) {
+        trySave((pages, repos, results, lang))
+        val repo = repos.dequeue()
+        val r = RepositoryFactory(repo)
+        if (r.jni) {
+          println(r.toString)
+          results += r.toString
+        }
+      }
+      trySave((pages, repos, results, lang))
+      val page = pages.dequeue()
+      val url = s"https://api.github.com/search/repositories?order=desc&page=$page&q=language:$lang&sort=stars"
+      val (res, rem, resetT) = get(url, RepositoryFactory.headers)
+      if (rem <= 1) Thread.sleep(resetT * 1000 - System.currentTimeMillis() + 2000)
+      repos ++=
+        res.parseJson.asJsObject.fields("items").asInstanceOf[JsArray].elements.map(_.asJsObject.fields("url").asInstanceOf[JsString].value)
+    }
+  }
+
+
+  def get(url: String, headers: Map[String, String] = Map()): (String, Int, Int) = {
     val con = (new URL(url)).openConnection.asInstanceOf[HttpsURLConnection]
     headers.map{ case (k, v) => con.setRequestProperty(k, v) }
     con.setUseCaches(false)
@@ -45,34 +83,37 @@ object Main {
     val in = con.getInputStream
     val content = IOUtils.toString(in, "UTF-8")
     in.close()
+    val rem = con.getHeaderField("X-RateLimit-Remaining").toInt
+    val resetT = con.getHeaderField("X-RateLimit-Reset").toInt
+    //println((rem, resetT))
     con.disconnect()
-    content
+    (content, rem, resetT)
   }
 }
 
-case class Repository(href: String) {
-  import Repository._
-
-  val languages: Map[String, Int] =
-    Main.get(s"$href/languages", headers)
-      .parseJson
-      .asJsObject
-      .fields
-      .map{ case (k, v) => k.toLowerCase -> v.prettyPrint.toInt }
-
-  val lines: Int = languages.values.sum
-
-  val java: Double = languages.getOrElse("java", 0) * 1.0 / lines
-  val c: Double = languages.getOrElse("c", 0) * 1.0 / lines
-  val cpp: Double = languages.getOrElse("c++", 0) * 1.0 / lines
-
-  val jni: Boolean = java > 0 && (c > 0 || cpp > 0)
-
-  override def toString: String = s"$href java($java) c($c) c++($cpp)"
+case class Repository(href: String, val java: Double, val c: Double, val cpp: Double) {
+  val jni: Boolean = java > 0 && c > 0 && cpp < 0.00001
 }
 
-case object Repository extends (String => Repository) {
+object RepositoryFactory {
   val headers = Map(
     "Authorization" -> s"token ${scala.io.Source.fromFile("token").mkString.trim}"
   )
+
+  def apply(href: String): Repository = {
+    val (res, rem, resetT) = Main.get(s"$href/languages", headers)
+    if (rem <= 1) Thread.sleep(resetT * 1000 - System.currentTimeMillis() + 2000)
+
+    val languages: Map[String, Int] =
+        res.parseJson
+        .asJsObject
+        .fields
+        .map{ case (k, v) => k.toLowerCase -> v.prettyPrint.toInt }
+      val lines: Int = languages.values.sum
+
+    val java: Double = languages.getOrElse("java", 0) * 1.0 / lines
+    val c: Double = languages.getOrElse("c", 0) * 1.0 / lines
+    val cpp: Double = languages.getOrElse("c++", 0) * 1.0 / lines
+    Repository(href, java, c, cpp)
+  }
 }
